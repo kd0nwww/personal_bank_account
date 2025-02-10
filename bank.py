a128a79482d7c1a5c65aa527898e5250ad46c412
Kirill Donetskov <kirilldonetskov@gmail.com>
2025-02-11 02:53:48 +0600

    Minor improvements

--- bank.py
+++ bank.py
@@ -1,26 +1,27 @@
 class Amount:
-    def __init__(self, amount, timestamp, transaction_type):
-        self.amount = amount
+    def __init__(self, amount: float, timestamp: str, transaction_type: str):
+        self.amount = amount    
         self.timestamp = timestamp
         self.transaction_type = transaction_type
 
     def __str__(self):
-        return f"Transaction: {self.transaction_type}\nAmount: {self.amount}\nProccessed at: {self.timestamp}"
-    
+        return f"Transaction: {self.transaction_type}\nAmount: {self.amount}\nDate: {self.timestamp.split(' ')[0]}\nTime: {self.timestamp.split(' ')[1]}"
+
+
 class PersonalAccount:
-    def __init__(self, account_number, account_holder, balance=0.0, transactions=[]):
+    def __init__(self, account_number: int, account_holder: str, balance: float=0.0, transactions: list[Amount]=[]):
         self.__account_number = account_number
         self.__account_holder = account_holder
         self.__balance = balance
         self.transactions = transactions
 
-    def deposit(self, amount):
+    def deposit(self, amount: float):
         from datetime import datetime
         transaction = Amount(amount, datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "DEPOSIT")
         self.__balance += amount
         self.transactions.append(transaction)
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float):
         if self.__balance - amount < 0:
             print("Insufficient funds.")
         else:
@@ -29,7 +30,6 @@ class PersonalAccount:
             self.__balance -= amount
             self.transactions.append(transaction)
             
-
     def print_transaction_history(self):
         if not self.transactions:
             print("You don't have any transactions.")
@@ -38,9 +38,7 @@ class PersonalAccount:
             for transaction in self.transactions:
                 if isinstance(transaction, Amount):
                     print("---" * 10)
-                    print(f"Transaction type: {transaction.transaction_type}")
-                    print(f"Amount: {transaction.amount}")
-                    print(f"Date: {transaction.timestamp}")
+                    print(str(transaction))
 
     def get_balance(self):
         return self.__balance
@@ -48,22 +46,22 @@ class PersonalAccount:
     def get_account_number(self):
         return self.__account_number
     
-    def set_account_number(self, account_number):
+    def set_account_number(self, account_number: int):
         self.__account_number = account_number
 
     def get_account_holder(self):
         return self.__account_holder
     
-    def set_account_holder(self, account_holder):
+    def set_account_holder(self, account_holder: str):
         self.__account_holder = account_holder
 
     def __str__(self):
         return f"Account number: {self.__account_number}\nAccount holder: {self.__account_holder}\nBalance: {self.__balance}"
     
-    def __add__(self, amount):
+    def __add__(self, amount: Amount):
         self.__balance += amount.get_balance()
 
-    def __sub__(self, amount):
+    def __sub__(self, amount: Amount):
         if self.__balance - amount.get_balance() < 0:
             print("Insufficient funds.")
         else:
